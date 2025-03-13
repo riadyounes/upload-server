55ed38b6d4b6b041c4e04d040e37504cba1b2911
Riad Younes <riad.younes@hotmail.com>
2025-03-13 20:28:46 -0300

    feat: add upload image route

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,9 +1,33 @@
+import { uploadImageRoute } from '@/infra/http/routes/upload-image'
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
 
 const server = fastify()
 
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.message,
+    })
+  }
+
+  console.log(error)
+
+  return reply.status(500).send({ message: 'Internal server Error' })
+})
+
 server.register(fastifyCors, { origin: '*' })
+
+server.register(uploadImageRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('Server is running on port 3333')
