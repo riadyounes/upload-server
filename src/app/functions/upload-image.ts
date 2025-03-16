d88da746afddf53f3ea37b4d67f82da25e0661f8
Riad Younes <riad.younes@hotmail.com>
2025-03-16 12:08:53 -0300

    feat: implement error handling for invalid file formats in image upload

--- src/app/functions/upload-image.ts
+++ src/app/functions/upload-image.ts
@@ -2,7 +2,9 @@ import { Readable } from 'node:stream'
 import { db } from '@/infra/db'
 import { schema } from '@/infra/db/schemas'
 import { uploads } from '@/infra/db/schemas/uploads'
+import { type Either, makeLeft, makeRight } from '@/shared/either'
 import { z } from 'zod'
+import { InvalidFileFormat } from './errors/invalid-file-format'
 
 const uploadImageInput = z.object({
   fileName: z.string(),
@@ -14,11 +16,13 @@ export type UploadImageInput = z.input<typeof uploadImageInput>
 
 const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/jpg', 'image/webp']
 
-export async function uploadImage(input: UploadImageInput) {
+export async function uploadImage(
+  input: UploadImageInput
+): Promise<Either<InvalidFileFormat, { url: string }>> {
   const { contentStream, contentType, fileName } = uploadImageInput.parse(input)
 
   if (!allowedMimeTypes.includes(contentType)) {
-    throw new Error('Invalid file format.')
+    return makeLeft(new InvalidFileFormat())
   }
 
   await db.insert(schema.uploads).values({
@@ -26,4 +30,6 @@ export async function uploadImage(input: UploadImageInput) {
     remoteKey: fileName,
     remoteUrl: fileName,
   })
+
+  return makeRight({ url: '' })
 }
