00cbed72bb9454a3ce0fb3e88df2f3d1f6b41250
Riad Younes <riad.younes@hotmail.com>
2025-03-16 22:24:30 -0300

    feat: implement getUploads function for retrieving and paginating uploads with optional search and sorting

---
+++ src/app/functions/get-uploads.ts
@@ -0,0 +1,51 @@
+import { db } from '@/infra/db'
+import { schema } from '@/infra/db/schemas'
+import { makeRight } from '@/shared/either'
+import { asc, count, desc, ilike } from 'drizzle-orm'
+import { z } from 'zod'
+
+const getUploadsInput = z.object({
+  searchQuery: z.string().optional(),
+  sortBy: z.enum(['createdAt']).optional(),
+  sortDirection: z.enum(['asc', 'desc']).optional(),
+  page: z.number().optional().default(1),
+  pageSize: z.number().optional().default(20),
+})
+
+type GetUploadsInput = z.input<typeof getUploadsInput>
+
+export async function getUploads(input: GetUploadsInput) {
+  const { page, pageSize, searchQuery, sortBy, sortDirection } =
+    getUploadsInput.parse(input)
+
+  const [uploads, [{ total }]] = await Promise.all([
+    db
+      .select()
+      .from(schema.uploads)
+      .where(
+        searchQuery ? ilike(schema.uploads.name, `%${searchQuery}%`) : undefined
+      )
+      .orderBy(fields => {
+        if (sortBy && sortDirection === 'asc') {
+          return asc(fields[sortBy])
+        }
+
+        if (sortBy && sortDirection === 'desc') {
+          return desc(fields[sortBy])
+        }
+
+        return desc(fields.id)
+      })
+      .offset((page - 1) * pageSize)
+      .limit(pageSize),
+
+    db
+      .select({ total: count(schema.uploads.id) })
+      .from(schema.uploads)
+      .where(
+        searchQuery ? ilike(schema.uploads.name, `%${searchQuery}%`) : undefined
+      ),
+  ])
+
+  return makeRight({ uploads, total })
+}
